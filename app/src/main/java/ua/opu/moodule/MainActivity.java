9eee9ab0c9806cdba229a1c2de7c03edd61d7b05
dieundead <dieundead2212@gmail.com>
2021-04-17 01:06:30 +0300

    Fixed camera

--- app/src/main/java/ua/opu/moodule/MainActivity.java
+++ app/src/main/java/ua/opu/moodule/MainActivity.java
@@ -1,21 +1,40 @@
 package ua.opu.moodule;
 
+import androidx.annotation.ColorLong;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final int IMAGE_CAPTURE_REQUEST_CODE = 7777;
+    private ImageView profileImage;
+    private Uri mContactImageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileImage = (ImageView) findViewById(R.id.img);
+
 
         ImageButton ib = (ImageButton) findViewById(R.id.imageButton3);
         ib.setOnClickListener(new View.OnClickListener() {
@@ -24,6 +43,17 @@ public class MainActivity extends AppCompatActivity {
                 openQuitDialog();
             }
         });
+
+        ImageButton takePhotoButton = (ImageButton) findViewById(R.id.imageButton);
+        takePhotoButton.setOnClickListener(v -> {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            try {
+                startActivityForResult(takePictureIntent, IMAGE_CAPTURE_REQUEST_CODE);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(this, "Error while trying to open camera app", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 
     private void openQuitDialog() {
@@ -47,8 +77,36 @@ public class MainActivity extends AppCompatActivity {
         quitDialog.show();
     }
 
-    public void onCameraClick(View view) {
-        Toast.makeText(this, "Не успел приделать камеру(((", Toast.LENGTH_SHORT).show();
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == IMAGE_CAPTURE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+            profileImage.setImageBitmap(imageBitmap);
+
+            String filename = "profile_" + System.currentTimeMillis() + ".png";
+
+            File outputFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), filename);
+            FileOutputStream fileOutputStream = null;
+
+            ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
+            ib.setVisibility(View.INVISIBLE);
+            try {
+                fileOutputStream = new FileOutputStream(outputFile);
+                imageBitmap.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+
+                mContactImageUri = Uri.fromFile(outputFile);
+                fileOutputStream.flush();
+                fileOutputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 
 
